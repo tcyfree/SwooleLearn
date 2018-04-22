1b4db5b31f6f03005cd2aef7df72deb7c70f060e
tcyfree <1946644259@qq.com>
2018-04-22 23:42:32 +0800

    补充内容

--- demo/server/tcp.php
+++ demo/server/tcp.php
@@ -3,8 +3,9 @@
 //创建Server对象，监听 127.0.0.1:9501端口
 $serv = new swoole_server("127.0.0.1", 9501);
 
+//swoole_server->set函数用于设置swoole_server运行时的各项参数
 $serv->set([
-    'worker_num' => 6 , // worker进程数 cpu 1-4
+    'worker_num' => 6 , // worker进程数 cpu 1-4倍
     'max_request' => 10000,
 ]);
 //监听连接进入事件
@@ -17,6 +18,9 @@ $serv->on('connect', function ($serv, $fd, $reactor_id) {
 });
 
 //监听数据接收事件
+/**
+ * $reactor_id = $from_id
+ */
 $serv->on('receive', function ($serv, $fd, $reactor_id, $data) {
     $serv->send($fd, "Server: {$reactor_id} - {$fd}".$data);
 });
