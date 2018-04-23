311cf5636b0ffcc26d2064b39e256c765d7feb72
tcyfree <1946644259@qq.com>
2018-04-23 23:07:10 +0800

    http_server:配置静态文件根目录

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -13,6 +13,13 @@ $http = new swoole_http_server("0.0.0.0", 8811);
 //    $response->end('sss'.json_encode($request->get));
 //});
 
+/**
+ * https://wiki.swoole.com/wiki/page/783.html
+ * 配置静态文件根目录，与enable_static_handler配合使用。
+ * 设置document_root并设置enable_static_handler为true后，
+ * 底层收到Http请求会先判断document_root路径下是否存在此文件，
+ * 如果存在会直接发送文件内容给客户端，不再触发onRequest回调。
+ */
 $http->set(
     [
         'enable_static_handler' => true,
