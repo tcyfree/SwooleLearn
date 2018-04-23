f10c12d6343a354772b22f2cda29138124f5db01
tcyfree <1946644259@qq.com>
2018-04-23 11:57:38 +0800

    还原

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -7,27 +7,27 @@
  */
 $http = new swoole_http_server("0.0.0.0", 8811);
 
-//$http->set(
-//    [
-//        'enable_static_handler' => true,
-//        'document_root' => "/home/work/hdtocs/swoole_mooc/data",
-//    ]
-//);
-//$http->on('request', function($request, $response) {
-//    //print_r($request->get);
-//    $content = [
-//        'date:' => date("Ymd H:i:s"),
-//        'get:' => $request->get,
-//        'post:' => $request->post,
-//        'header:' => $request->header,
-//    ];
-//
-//    swoole_async_writefile(__DIR__."/access.log", json_encode($content).PHP_EOL, function($filename){
-//        // todo
-//    }, FILE_APPEND);
-//    $response->cookie("singwa", "xsssss", time() + 1800);
-//    $response->end("sss". json_encode($request->get));
-//});
+$http->set(
+    [
+        'enable_static_handler' => true,
+        'document_root' => "/home/work/hdtocs/swoole_mooc/data",
+    ]
+);
+$http->on('request', function($request, $response) {
+    //print_r($request->get);
+    $content = [
+        'date:' => date("Ymd H:i:s"),
+        'get:' => $request->get,
+        'post:' => $request->post,
+        'header:' => $request->header,
+    ];
+
+    swoole_async_writefile(__DIR__."/access.log", json_encode($content).PHP_EOL, function($filename){
+        // todo
+    }, FILE_APPEND);
+    $response->cookie("singwa", "xsssss", time() + 1800);
+    $response->end("sss". json_encode($request->get));
+});
 
 $http->on('request', function ($request, $response) {
     $response->cookie("singwa",'xsssss', time() + 1800);
