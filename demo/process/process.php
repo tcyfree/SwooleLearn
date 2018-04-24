4105a31471f1ab5e44729af541bf968e4bf96b67
tcy <1946644259@qq.com>
2018-04-24 23:06:46 +0800

    回收结束运行的子进程

--- demo/process/process.php
+++ demo/process/process.php
@@ -7,11 +7,11 @@
  */
 $process = new swoole_process(function(swoole_process $pro) {
     // todo
-    // php redis.php
-    $pro->exec("/home/work/study/soft/php/bin/php", [__DIR__.'/../server/http_server.php']);
+//     php redis.php
+    $pro->exec("/usr/local/php/bin/php", [__DIR__.'/../server/http_server.php']);
 }, false);
 
 $pid = $process->start();
 echo $pid . PHP_EOL;
-
+//回收结束运行的子进程
 swoole_process::wait();
